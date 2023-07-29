4f36ec3060a3581d1ecf1a277ae39342857495d3
Cetav-B6-17 <106450335+ValeriaAcuM@users.noreply.github.com>
2023-07-29 12:02:25 -0600

    structure cards

---
+++ src/js/modules/cards.js
@@ -0,0 +1,8 @@
+function renderCards(data) {
+  const panels = document.getElementsByClassName('card-container');
+  const cards = data.map(element => {
+    const {title, image, name, soil, pot, color, extras} = element;
+
+    return 
+  })
+}
