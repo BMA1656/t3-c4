bb9d1eb190f037f9ac1191dff6405bfaf8416e3b
ValeriaAcuM <106450335+ValeriaAcuM@users.noreply.github.com>
2023-08-17 23:59:12 -0600

    Added Observer

--- src/js/config/details.js
+++
@@ -1,7 +0,0 @@
-import { colorChange} from './pot.js';
-
-const initDetails = () => {
-  colorChange.subscribe(handleColorChange);
-}
-
-export {initDetails};
--- src/js/config/extras.js
+++ src/js/config/extras.js
@@ -1,14 +1,30 @@
 import Publisher from "./publisher.js";
 
+const originalPlantData = localStorage.getItem("localPlant");
+const originalPlant = JSON.parse(originalPlantData);
+
 const extrasChange = new Publisher();
 
-const handleSoilChange = (event) => {
-  //data-id = aglaonema
-  extraChange.publish(plant)
+const handleExtrasChange = () => {
+  const inputExtras = document.querySelectorAll(".input-extra");
+  inputExtras.forEach((input) => {
+    input.addEventListener('change', (event) => {
+      const extra = event.currentTarget.dataset.id;
+      extrasChange.publish(extra);
+      console.log("extras", extra);
+    });
+  })
 };
 
-const initExtra = () => {
-  extraInputs.forEach(elementsEx => {
-    elementsEx.addEventListener('change', handleSoilChange);
-  });
+function renderExtrasChange(extra) {
+  console.log("renderExtrasChange", extra);
+  // IMAGEN
+  originalPlant.elements = extra; // NO SIRVE
 }
+
+function initExtras() {
+  extrasChange.subscribe(renderExtrasChange);
+  handleExtrasChange();
+}
+
+export default initExtras;
