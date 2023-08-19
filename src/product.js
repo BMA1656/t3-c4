ffa98d96d6177fc3f4ea170cc51155a2f3b63e5f
BmLatin0 <73851025+BMA1656@users.noreply.github.com>
2023-08-19 02:46:22 -0600

    base part of the final

--- src/product.js
+++ src/product.js
@@ -5,3 +5,17 @@ import { localStorageObject } from "./js/components/localstorage";
 document.addEventListener('DOMContentLoaded',() => { 
     renderCards(localStorageObject());
   })
+
+
+  const detailsContainers = document.querySelectorAll("#dataContainer details");
+  detailsContainers.forEach((detailsElement) => {
+      detailsElement.addEventListener("toggle", function() {
+          if (this.open) {
+              detailsContainers.forEach((otherDetailsElement) => {
+                  if (otherDetailsElement !== this) {
+                      otherDetailsElement.open = false;
+                  }
+              });
+          }
+      });
+  });
